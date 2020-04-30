4a711f57e2fd4a1b2382a6b976a2c918fc666528
Christoph Welcz <christoph@welcz.de>
2020-04-30 14:48:48 +0200

    removed prettier as it is worse than the default formatting from webstorm.

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const command = argv[2];
 const category = argv[3];
 
 module.exports = () => {
-    switch (command) {
-        case 'joke':
-            if (!category) {
-                req.requestJoke().then(console.log);
-            } else {
-                req.requestJokeForCategory(category)
-                    .then(console.log)
-                    .catch((error) => console.error(error.message));
-            }
-            break;
-        case 'categories':
-            req.requestCategories().then(console.log);
-            break;
-    }
+  switch (command) {
+    case 'joke':
+      if (!category) {
+        req.requestJoke().then(console.log);
+      } else {
+        req.requestJokeForCategory(category)
+           .then(console.log)
+           .catch((error) => console.error(error.message));
+      }
+      break;
+    case 'categories':
+      req.requestCategories().then(console.log);
+      break;
+  }
 };
--- src/requests.js
+++ src/requests.js
@@ -1,36 +1,36 @@
 const fetch = require('node-fetch');
 
 const requestJoke = async () => {
-    const url = 'https://api.chucknorris.io/jokes/random';
-    const response = await fetch(url);
-    const data = await response.json();
-    return data.value;
+  const url = 'https://api.chucknorris.io/jokes/random';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.value;
 };
 
 const requestJokeForCategory = async (category) => {
-    const categories = await requestCategories();
-    const [found] = await categories.filter((c) => c === category);
-    if (!found) {
-        throw new Error(`category ${category} not found!`);
-    }
-    return requestSpecificCategory(found);
+  const categories = await requestCategories();
+  const [found] = await categories.filter((c) => c === category);
+  if (!found) {
+    throw new Error(`category ${category} not found!`);
+  }
+  return requestSpecificCategory(found);
 };
 
 const requestCategories = async () => {
-    const url = 'https://api.chucknorris.io/jokes/categories';
-    const response = await fetch(url);
-    return await response.json();
+  const url = 'https://api.chucknorris.io/jokes/categories';
+  const response = await fetch(url);
+  return await response.json();
 };
 
 const requestSpecificCategory = async (category) => {
-    const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data.value;
+  const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.value;
 };
 
 module.exports = {
-    requestJoke,
-    requestJokeForCategory,
-    requestCategories,
+  requestJoke,
+  requestJokeForCategory,
+  requestCategories,
 };
