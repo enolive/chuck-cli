c5604f4ca8ff6bbe6f2fef9bfda3c253dc0a3cbf
Christoph Welcz <christoph@welcz.de>
2020-05-01 11:37:46 +0200

    :white_check_mark: written meaningful tests

--- __tests__/requests.test.js
+++ __tests__/requests.test.js
@@ -1,21 +1,67 @@
+const fetchMock = require('jest-fetch-mock');
 const req = require('../src/requests.js');
-const fetch = require('node-fetch');
 
-/*
-    These are not the real tests, I'm still having some problems with
-    test API calls with fetch. 
-*/
+beforeEach(() => fetchMock.resetMocks());
 
-describe('Requests', () => {
-    it('Request joke should return a joke.', async () => {
-        expect(req.requestJoke()).toBeDefined();
-    });
+test('Request joke calls API', () => {
+  req.requestJoke();
 
-    it('Request categories should return all the categories.', async () => {
-        expect(req.requestCategories()).toBeDefined();
-    });
+  expect(fetchMock).toHaveBeenCalledTimes(1);
+  expect(fetchMock).toHaveBeenCalledWith('https://api.chucknorris.io/jokes/random');
+});
 
-    it('Request categorie should return a specific joke based on category.', async () => {
-        expect(req.requestJokeForCategory('history')).toBeDefined();
-    });
+const mockReturnedJoke = (jokeText) => {
+  fetchMock.mockResponseOnce(`{
+    "value": "${jokeText}"
+  }`);
+};
+
+test('Request joke should return a joke.', async () => {
+  mockReturnedJoke('Here is the joke');
+  await expect(req.requestJoke()).resolves.toEqual('Here is the joke');
+});
+
+test('Request categories calls the API', () => {
+  req.requestCategories();
+
+  expect(fetchMock).toHaveBeenCalledTimes(1);
+  expect(fetchMock).toHaveBeenCalledWith('https://api.chucknorris.io/jokes/categories');
+});
+
+const mockReturnedCategories = categories => {
+  fetchMock.mockResponseOnce(categories);
+};
+
+test('Request categories returns all the categories.', async () => {
+  // language=JSON
+  mockReturnedCategories('["first", "second"]');
+
+  await expect(req.requestCategories()).resolves.toEqual(['first', 'second']);
+});
+
+test('Request joke for category calls the API.', async () => {
+  // language=JSON
+  mockReturnedCategories('["first", "second"]');
+  mockReturnedJoke('The joke');
+  await req.requestJokeForCategory('first');
+
+  expect(fetchMock).toHaveBeenCalledTimes(2);
+  expect(fetchMock).toHaveBeenNthCalledWith(1, 'https://api.chucknorris.io/jokes/categories');
+  expect(fetchMock).toHaveBeenNthCalledWith(2, 'https://api.chucknorris.io/jokes/random?category=first');
+});
+
+test('Request joke for category returns the expected joke.', async () => {
+  // language=JSON
+  mockReturnedCategories('["first", "second"]');
+  mockReturnedJoke('The joke');
+
+  await expect(req.requestJokeForCategory('first')).resolves.toEqual('The joke');
+});
+
+test('Request joke for not existing category fails.', async () => {
+  // language=JSON
+  mockReturnedCategories('["first", "second"]');
+
+  await expect(req.requestJokeForCategory('not here')).rejects.toEqual(new Error('category not here not found!'));
+  expect(fetchMock).toHaveBeenCalledTimes(1);
 });
